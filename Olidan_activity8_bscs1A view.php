7496edb9d1f7a5e2eeca04f2ec896c0b81902b63
dayvolids222 <137800059+dayvolids222@users.noreply.github.com>
2023-06-27 00:49:29 +0800

    Add files via upload

---
+++ Olidan_activity8_bscs1A view.php
@@ -0,0 +1,88 @@
+<?php
+$servername = "127.0.0.1"; // Replace with the actual MySQL server address
+$username = "root"; // Replace with your MySQL username
+$password = ""; // Replace with your MySQL password
+$database = "act08"; // Replace with the name of your database
+
+// Create a connection
+$conn = new mysqli($servername, $username, $password, $database);
+
+// Check connection
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+}
+
+// Retrieve data from the database
+$sql = "SELECT name, email, MESSEGE items";
+$result = $conn->query($sql);
+?>
+<!DOCTYPE html>
+<html>
+<head>
+  <title>Retrieved Form Data</title>
+  <style>
+    /* Custom styling for the retrieved form data */
+    body {
+      font-family: Arial, sans-serif;
+      margin: 20px;
+    }
+
+    h2 {
+      color: #333;
+    }
+
+    .form-data {
+      background-color: #f5f5f5;
+      padding: 10px;
+      margin-bottom: 20px;
+      border: 1px solid #ccc;
+      border-radius: 4px;
+    }
+
+    .form-data p {
+      margin: 0;
+    }
+
+    hr {
+      margin: 20px 0;
+      border: 0;
+      border-top: 1px solid #ccc;
+    }
+
+    .btn {
+      display: inline-block;
+      padding: 10px 20px;
+      background-color: #4c55af;
+      color: #fff;
+      text-decoration: none;
+      border-radius: 4px;
+      transition: background-color 0.3s ease;
+    }
+
+    .btn:hover {
+      background-color: #45a049;
+    }
+  </style>
+</head>
+<body>
+  <h2>Retrieved Form Data</h2>
+  <?php
+  if ($result->num_rows > 0) {
+    while ($row = $result->fetch_assoc()) {
+      echo "<div class='form-data'>";
+      echo "<p><strong>Name:</strong> " . $row["name"] . "</p>";
+      echo "<p><strong>Email:</strong> " . $row["email"] . "</p>";
+      echo "<p><strong>Message:</strong> " . $row["message"] . "</p>";
+      echo "</div>";
+      echo "<hr>";
+    }
+  } else {
+    echo "<p>No form data found.</p>";
+  }
+
+  // Close the connection
+  $conn->close();
+  ?>
+  <a href="Olidan_activity8_bscs1A.html" class="btn">New Entry</a>
+</body>
+</html>
---
+++ Olidan_activity8_bscs1A.sql
@@ -0,0 +1,9 @@
+CREATE TABLE items (
+  id INT AUTO_INCREMENT PRIMARY KEY,
+  name VARCHAR(50) NOT NULL,
+  email VARCHAR(50) NOT NULL,
+  message TEXT NULL,
+  created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+);
+Footer
+© 2023 GitHub, Inc.
