ab9790eec3bd6e1170034c6c24bfe05a77b89b71
dayvolids222 <137800059+dayvolids222@users.noreply.github.com>
2023-06-26 23:45:39 +0800

    Add files via upload

---
+++ olidan/Olidan_activity8_bscs1A.php
@@ -0,0 +1,35 @@
+<?php
+$servername = "127.0.0.1"; // Replace with the actual MySQL server address
+$username = "root"; // Replace with your MySQL username
+$password = ""; // Replace with your MySQL password
+$database = "activity8"; // Replace with the name of your database
+
+// Create a connection
+$conn = new mysqli($servername, $username, $password, $database);
+
+// Check connection
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+}
+
+// Retrieve form data
+$name = $_POST['name'];
+$email = $_POST['email'];
+$message = $_POST['message'];
+
+// Prepare the INSERT statement
+$sql = "INSERT INTO items (name, email, message) VALUES (?, ?, ?)";
+$stmt = $conn->prepare($sql);
+$stmt->bind_param("sss", $name, $email, $message);
+
+// Execute the statement
+if ($stmt->execute()) {
+    echo "Form data saved successfully.";
+} else {
+    echo "Error: " . $stmt->error;
+}
+
+// Close the connection
+$stmt->close();
+$conn->close();
+?>
